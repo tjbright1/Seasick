0fbd5c83e10f0035698517fb578661a6c16ad7f8
Mitch McClellan <mitchelljmcclellan@gmail.com>
2015-01-30 09:28:57 -0500

    Added Pause menu and some font and other assets

--- Assets/Scripts/ToggleWireframe.js
+++ Assets/Scripts/ToggleWireframe.js
@@ -5,12 +5,12 @@ var isOn : boolean  = false;
 
 function Start()
 {
-	wireframeCamera.GetComponent(Camera).camera.enabled = isOn;
+//	wireframeCamera.GetComponent(Camera).camera.enabled = isOn;
 }
 
 function Update () 
 {
-	var b : boolean = wireframeCamera.GetComponent(Camera).camera.enabled;
+//	var b : boolean = wireframeCamera.GetComponent(Camera).camera.enabled;
 	
-	if(Input.GetKeyDown(KeyCode.F2)) wireframeCamera.GetComponent(Camera).camera.enabled = !b;
+//	if(Input.GetKeyDown(KeyCode.F2)) wireframeCamera.GetComponent(Camera).camera.enabled = !b;
 }

2dc61f77cba60aca1b8a8e9a762102e5d59d9b59
magipe <jymyj@naver.com>
2015-02-20 00:57:03 -0500

    Update CameraControl.cs

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -10,6 +10,7 @@ public class CameraControl : MonoBehaviour {
 	float horizAxis;
 	float vertAxis;
 	float mWheel;
+	int zoomSpeed = 4;
 	// Use this for initialization
 	void Start () {
 	}
@@ -18,11 +19,25 @@ public class CameraControl : MonoBehaviour {
 	void Update () {
 		horizAxis = Input.GetAxis ("Horizontal");
 		vertAxis = Input.GetAxis ("Vertical");
-		Vector3 vertTranslate = new Vector3(0, vertAxis * maxVertSpeed, 0);
+		mWheel = Input.GetAxis ("Mouse ScrollWheel");
+		Vector3 vertTranslate = new Vector3 (0, vertAxis * maxVertSpeed, 0);
 		Vector3 horizTranslate = new Vector3 (horizAxis * maxSpeed, 0, 0);
 		transform.Translate (horizTranslate);
 		Vector3 targetPos = target.transform.position;
 		transform.Translate (vertTranslate);
 		transform.LookAt (targetPos);
+		if (mWheel > 0) {
+			camera.fieldOfView = camera.fieldOfView - zoomSpeed;
+			if (camera.fieldOfView <= 40) {
+				camera.fieldOfView = 40;
+			}
+		} else if (mWheel < 0) {
+			camera.fieldOfView = camera.fieldOfView + zoomSpeed;
+			if (camera.fieldOfView >= 85) {
+				camera.fieldOfView = 85;
+			}
+//			Camera.main.fieldOfView = Mathf.Max (Camera.main.fieldOfView + 2, 85.0);
+		}
 	}
 }
+
